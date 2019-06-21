4d6459abd04fb6235fb792f608d4801a8ccc4ec0
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-20 22:47:55 -0500

    Update ndccservice.groovy

--- vars/ndccservice.groovy
+++ vars/ndccservice.groovy
@@ -8,24 +8,32 @@ stages {
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.
+	   
+	   steps {	   
       mvnHome = tool 'M3'
       gradleHome = tool 'gradle'
+	   }
    }
    stage('Build') {
       // Run the maven build
+	   steps {
       if (isUnix()) {
          sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
       } else {
          bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
       }
+	   }
    }
    stage('gradle build') {
+	   steps {
         if (isUnix()){
 		//sh "'${gradleHome}/bin/gradle clean build'"
 		sh "./gradlew clean build"
 		} else {
 		   bat(/"${gradleHome}\bin\gradle" clean build/)
 		   }
+	   }
 		   }
+	   
 }
 }
