0823f013d2f2c84fcdb5564945c1c32153f13cc3
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-20 22:54:44 -0500

    Update ndccservice.groovy

--- vars/ndccservice.groovy
+++ vars/ndccservice.groovy
@@ -2,14 +2,13 @@ pipeline {
 stages {
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
-		checkout scm
+		
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.
 	   
 	   steps {	   
-      def mvnHome = tool 'M3'
-      def gradleHome = tool 'gradle'
+		   println 'test here'
 	   }
    }
 }
