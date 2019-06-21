a6b6b595fed6691d0314fa841f19106ae12171ba
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-20 22:21:33 -0500

    Update ndccservice.groovy

--- vars/ndccservice.groovy
+++ vars/ndccservice.groovy
@@ -1,10 +1,34 @@
-#!/usr/bin/env groovy
-node {	
-   stage('Preparation') { // for display purposes
-      // Get some code from a GitHub repository
-      git'https://github.com/ndccroyals/ndcc-service-registration.git'
-      // Get the Maven tool.
-      // ** NOTE: This 'M3' Maven tool must be configured
-      // **       in the global configuration.            
+def triggerBuildArray = []
+    def buildJobArray = []
+    def jobArray = []
+    def paramsArray = []
+    def noOfJob = 2 
+
+    //function to trigger job
+
+    def triggerJob(def job, def params, def jobNo, def buildJobArray) {
+
+    buildJobArray << job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
+
+    println"triggered job "+jobNo;
+    println"waiting for completion of job "+jobNo;
+
    }
-}
+
+    jobArray << Hudson.instance.getJob('job1');
+
+
+    //define parameters
+
+    paramsArray << [            
+    new StringParameterValue('baseurl',build.getEnvironment(listener).get('ORAbaseurl')),
+    new StringParameterValue('firm',build.getEnvironment(listener).get('ORAfirm')),
+    new StringParameterValue('loginname',build.getEnvironment(listener).get('ORAloginname'))
+
+    ]
+
+    for(int i=0;i<noOfJob;i++)
+    {
+        triggerJob(jobArray[i],paramsArray[i],i+1, buildJobArray);
+
+    }
