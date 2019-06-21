19c1be1a73191c1cc0ee88a56a9cab5ce388acd1
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-20 22:39:55 -0500

    Update ndccservice.groovy

--- vars/ndccservice.groovy
+++ vars/ndccservice.groovy
@@ -1,1 +1,32 @@
-println "Hello World!"
+node {
+   def mvnHome
+   def gradleHome
+   stage('Preparation') { // for display purposes
+      // Get some code from a GitHub repository
+		checkout scm
+      // Get the Maven tool.
+      // ** NOTE: This 'M3' Maven tool must be configured
+      // **       in the global configuration.
+      mvnHome = tool 'M3'
+      gradleHome = tool 'gradle'
+   }
+   stage('Build') {
+      // Run the maven build
+      if (isUnix()) {
+         sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+      } else {
+         bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+      }
+   }
+   stage('gradle build') {
+        if (isUnix()){
+		//sh "'${gradleHome}/bin/gradle clean build'"
+		sh "./gradlew clean build"
+		} else {
+		   bat(/"${gradleHome}\bin\gradle" clean build/)
+		   }
+		   }
+      junit '---**/target/surefire-reports/TEST-*.xml'
+      archive 'target/*.jar'
+   }
+   }
