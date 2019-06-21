9396a52b250f5fc7f244a46085c989e80c1a0f61
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-20 22:53:21 -0500

    Update ndccservice.groovy

--- vars/ndccservice.groovy
+++ vars/ndccservice.groovy
@@ -12,30 +12,5 @@ stages {
       gradleHome = tool 'gradle'
 	   }
    }
-   stage('Build') {
-      // Run the maven build
-	   steps {
-		         mvnHome = tool 'M3'
-      gradleHome = tool 'gradle'
-      if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
-      } else {
-         bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-      }
-	   }
-   }
-   stage('gradle build') {
-	   steps {
-		         mvnHome = tool 'M3'
-      gradleHome = tool 'gradle'
-        if (isUnix()){
-		//sh "'${gradleHome}/bin/gradle clean build'"
-		sh "./gradlew clean build"
-		} else {
-		   bat(/"${gradleHome}\bin\gradle" clean build/)
-		   }
-	   }
-		   }
-	   
 }
 }
