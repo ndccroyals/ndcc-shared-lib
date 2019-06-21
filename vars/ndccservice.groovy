703e929cc741baaefd0f1c63f35e692a579fa927
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-20 22:20:13 -0500

    Update ndccservice.groovy

--- vars/ndccservice.groovy
+++ vars/ndccservice.groovy
@@ -1,40 +1,9 @@
 #!/usr/bin/env groovy
-node {
-   //def mvnHome
-   //def gradleHome
-   //def dockerHome
-	
+node {	
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
       git'https://github.com/ndccroyals/ndcc-service-registration.git'
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
-      // **       in the global configuration.           
-      mvnHome = tool 'M3'
-      gradleHome = tool 'gradle'
-      dockerHome = tool 'DOCKER_HOME'	   
+      // **       in the global configuration.            
    }
-   stage('Build') {
-      // Run the maven build
-      if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
-	      
-      } else {
-         bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-      }
-   }
-   stage('gradle build') {
-        if (isUnix()){
-		//sh "'${gradleHome}/bin/gradle clean'"
-		sh "./gradlew clean build"
-		sh "sudo /usr/local/bin/docker build -t ndcc ."
-		} else {
-		   bat(/"${gradleHome}\bin\gradle" clean build/)
-		   }
-		   }
-		   
-     // junit '**/target/surefire-reports/TEST-*.xml'
-     // archive 'target/*.jar'
-  // }
-   
-}
